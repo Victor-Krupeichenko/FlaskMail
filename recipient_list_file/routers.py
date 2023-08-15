db6760b7043f97db403f42bb72da27d5da9bc95f
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-15 22:12:59 +0300

    Эндпоинт для добавления получателей

--- recipient_list_file/routers.py
+++ recipient_list_file/routers.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, render_template, redirect, url_for
 from recipient_list_file.form_upload_file import FileUploadForms
 from recipient_list_file.utils import validate_file, inserting_data
+from mailing.utils import recipient_list
 
 recipient_router = Blueprint("recipient", __name__, template_folder="templates", static_folder="static")
 
@@ -12,7 +13,8 @@ def recipient_form():
     """Рендеринг формы и получение из формы файла со списком получателей"""
     form = FileUploadForms()
     if request.method == "GET":
-        return render_template("recipient_file_form.html", form=form)
+        recipients = recipient_list()
+        return render_template("recipient_file_form.html", form=form, recipients=recipients)
 
     file_path = request.files.get("file")
     result = validate_file(_SAVE_PATH_FILE, file_path, "email", "name")
