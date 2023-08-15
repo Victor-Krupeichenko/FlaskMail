a744d92a5c9d003d4caf44ea87af5e050277dc9c
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-15 22:09:28 +0300

    Рассылка писем

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -2,15 +2,17 @@ import smtplib
 from tasks.settings_celery import app_celery
 from settings_env import smtp_user, smtp_password, smtp_host, smtp_port
 from email.message import EmailMessage
+from tasks.utils import record_update
 
 
 @app_celery.task(queue="email")
 def send_email(emails_list, from_email, subject, text, file_data=None, file_name=None):
     """Рассылка писем"""
-    try:
-        with smtplib.SMTP_SSL(smtp_host, smtp_port) as server:
-            server.login(smtp_user, smtp_password)
-            for email in emails_list:
+
+    with smtplib.SMTP_SSL(smtp_host, smtp_port) as server:
+        server.login(smtp_user, smtp_password)
+        for email in emails_list:
+            try:
                 msg = EmailMessage()
                 msg["From"] = from_email
                 msg["To"] = email
@@ -19,6 +21,8 @@ def send_email(emails_list, from_email, subject, text, file_data=None, file_name
                 if file_data and file_name:
                     msg.add_attachment(file_data, maintype="application", subtype="octet-stream", filename=file_name)
                 server.send_message(msg)
-            return "Отправка завершена"
-    except (smtplib.SMTPException, ValueError) as ex:
-        return {"error": f"{ex}"}
+                record_update(email)
+            except (smtplib.SMTPException, ValueError) as ex:
+                print(f"error {ex}")
+                continue
+        return "Отправка завершена"
