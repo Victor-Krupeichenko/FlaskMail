57cf1a33c35c432ff6bdb4dea68e8b0bf54be461
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-15 16:40:55 +0300

    utils

--- recipient_list_file/utils.py
+++ recipient_list_file/utils.py
@@ -1,28 +1,7 @@
-import os
 from recipient_list_file.handlers import csv_handler, json_handler
-from settings_env import save_path_file
-from functools import wraps
+from my_utils import file_storage
 from database.models import Recipient
 from database.connect_db import session_maker
-
-
-def file_storage(func):
-    """Временное хранение файла и удаление файла после его чтения"""
-
-    @wraps(func)
-    def wrapper(file_path, *args, **kwargs):
-        if file_path:
-            save_path = save_path_file
-            if not os.path.exists(save_path):
-                os.makedirs(save_path)
-            temp_file_path = os.path.join(save_path, file_path.filename)
-            file_path.save(temp_file_path)
-            result = func(temp_file_path, *args, **kwargs)
-            os.remove(temp_file_path)
-            return result
-        return None
-
-    return wrapper
 
 
 @file_storage
