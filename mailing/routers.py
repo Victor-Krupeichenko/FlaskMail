338ac7b512e44f2b9260aeb1ea09a0d5b547596a
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-15 16:37:10 +0300

    Отправка писем получателям (вложение опционально в формате pdf)

--- mailing/routers.py
+++ mailing/routers.py
@@ -22,18 +22,21 @@ def send_email_to_recipient():
     form = MailForm()
     if request.method == "GET":
         return render_template("mailing_form.html", form=form)
-    elif request.method == "POST":
-        if form.validate_on_submit():
-            form_data = request.form.to_dict()
-            subject = form_data.get("subject")
-            text = form_data.get("text")
-            file = request.files.get("file")
-            email_list = email_recipient()
-            if valid_format_file(file.filename):
-                data = reading_file(_SAVE_PATH_FILE, file)
+
+    if form.validate_on_submit():
+        form_data = request.form.to_dict()
+        subject = form_data.get("subject")
+        text = form_data.get("text")
+        file = request.files.get("file")
+        email_list = email_recipient()
+        if valid_format_file(file.filename):
+            data = reading_file(_SAVE_PATH_FILE, file)
+            if not isinstance(data, str):
                 file_data = data.get("file_data")
                 file_name = data.get("file_name")
                 send_email.delay(email_list, smtp_user, subject, text, file_data=file_data, file_name=file_name)
             else:
                 send_email.delay(email_list, smtp_user, subject, text)
+        else:
+            send_email.delay(email_list, smtp_user, subject, text)
     return redirect(url_for("mailing.monitoring_recipient"))
