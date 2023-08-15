9cd0338586a05354f691c03592998bdf20df76e0
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-15 18:00:36 +0300

    Delete utils.py

--- tasks/utils.py
+++
@@ -1,9 +0,0 @@
-def reading_file(file_path):
-    """Читает файл для отправки по email"""
-    try:
-        with open(file_path, "rb") as file:
-            file_data = file.read()
-            file_name = file.name
-            return {"file_data": file_data, "file_name": file_name}
-    except Exception as ex:
-        return f"error: {ex}"
