50572cb6902edf801081dc5e992408ecd7047d82
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-16 15:48:12 +0300

    Добавлено регистрация эндпоинтов для получения данных для получателей

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from report.routers import report
 from recipient_list_file.routers import recipient_router
 from mailing.routers import mailing_list
 from settings_env import secrets_key_csrf
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.secret_key = secrets_key_csrf
 app.register_blueprint(recipient_router)
 app.register_blueprint(mailing_list)
+app.register_blueprint(report)
 
 
 @app.route("/")
