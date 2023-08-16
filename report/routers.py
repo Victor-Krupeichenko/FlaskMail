7b835159396cb97570f4595ed803849f56f3e219
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-16 15:45:00 +0300

    Эндпоинты получение данных получателя и скачать pdf файл с этими данными

--- report/routers.py
+++ report/routers.py
@@ -1,11 +1,12 @@
 import os
 from flask import Blueprint, render_template, request, redirect, url_for, send_file
 from report.forms import ReportForm
-from report.utils import get_report_recipient, create_pdf_report
+from report.utils import get_report_recipient, serialize_recipient
+from tasks.tasks import create_pdf_report
 
 report = Blueprint("report", __name__, template_folder="templates", static_folder="static")
 
-_SAVE_PATH_FILE = "report"
+_SAVE_PATH_FILE = "report/"
 _FILE_NAME = "recipient_list.pdf"
 
 
@@ -18,13 +19,17 @@ def report_recipient():
     if form.validate_on_submit():
         name = form.data.get("report")
         recipients = get_report_recipient(name)
-        create_pdf_report(recipients, save_folder_file=_SAVE_PATH_FILE, file_name=_FILE_NAME)
+        if recipients:
+            result = serialize_recipient(recipients)
+            create_pdf_report.delay(result, save_folder_file=_SAVE_PATH_FILE, file_name=_FILE_NAME)
         return render_template("report_to_recipient.html", form=form, recipients=recipients)
     return redirect(url_for("home"))
 
 
 @report.route("/download_report", methods=["GET"])
 def download_report():
-    """Скачать файл report"""
+    """Скачать файл report и потом его удалить"""
     pdf_path = os.path.join(_SAVE_PATH_FILE, _FILE_NAME)
-    return send_file(pdf_path, as_attachment=True)
+    response = send_file(pdf_path, as_attachment=True)
+    os.remove(pdf_path)
+    return response
